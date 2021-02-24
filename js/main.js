bc47b56a125fc5922b55ddf9d862e061cf13e7d1
dairbek <dairsadru@gmail.com>
2021-02-24 19:02:04 +0300

    git init

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
+const products = [{
+        id: 1,
+        title: 'Notebook',
+        price: 20000
+    },
+    {
+        id: 2,
+        title: 'Mouse',
+        price: 1500
+    },
+    {
+        id: 3,
+        title: 'Keyboard',
+        price: 5000
+    },
+    {
+        id: 4,
+        title: 'Gamepad',
+        price: 4500
+    },
+    {
+        id: 4,
+        title: 'Gamepad',
+        price: 4500
+    },
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title = "title", price = "price") => {
     return `<div class="product-item">
                 <h3>${title}</h3>
                 <p>${price}</p>
@@ -19,7 +39,7 @@ const renderProducts = (list) => {
     });
 
     console.log(productList);
-    document.querySelector('.products').innerHTML = productList;
+    document.querySelector('.products').innerHTML = productList.join("");
 }
 
 renderProducts(products);
